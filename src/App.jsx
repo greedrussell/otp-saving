b68d28d02871be8cb9bd45b66266f86bc91b8b4d
Russell Greed <greedrussell@gmail.com>
2020-07-22 15:55:45 +0300

    feat(hero-img): add section hero-img

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import React from './components/customer-info/node_modules/react'
+import React from 'react'
 
 import './App.css'
 import HeroImg from './components/hero-img/hero-img.component'
-import Footer from './components/footer/footer.component'
-import AboutAction from './components/about-action/about-action.components'
-import Calculator from './components/calculator/calculator.component'
-import HowItWork from './components/how-it-work/how-it-work.component'
-import CustomerInfo from './components/customer-info/customer-info.component'
-import Faq from './components/faq/faq.component'
-import MobileApp from './components/mobile-app/mobile-app.component'
+// import Footer from './components/footer/footer.component'
+// import AboutAction from './components/about-action/about-action.components'
+// import Calculator from './components/calculator/calculator.component'
+// import HowItWork from './components/how-it-work/how-it-work.component'
+// import CustomerInfo from './components/customer-info/customer-info.component'
+// import Faq from './components/faq/faq.component'
+// import MobileApp from './components/mobile-app/mobile-app.component'
 
 const App = () => {
 	return (
 		<>
 			<HeroImg />
-			<AboutAction />
-			<Calculator />
-			<HowItWork />
-			<CustomerInfo />
-			<Faq />
-			<MobileApp />
-			<Footer />
+			{/* <AboutAction /> */}
+			{/* <Calculator /> */}
+			{/* <HowItWork /> */}
+			{/* <CustomerInfo /> */}
+			{/* <Faq /> */}
+			{/* <MobileApp /> */}
+			{/* <Footer /> */}
 		</>
 	)
 }
--- src/components/hero-img/hero-img.component.jsx
+++ src/components/hero-img/hero-img.component.jsx
@@ -1,32 +1,40 @@
-import React from '../customer-info/node_modules/react'
+import React from 'react'
+import { Link } from 'react-scroll'
 
 import './hero-img.css'
+import LogoSVG from '../../assets/img/logo.svg'
+import ArrowSVG from '../../assets/img/hero-img__arrow.svg'
 
 const HeroImgPluses = () => (
-	<ul className="HeroImgPluses">
-		<li className="HeroImgPluses__item">
-			<img src="" alt="icon" />
-			<p>До 6,5% годовых</p>
+	<ul className="HeroImg__list">
+		<li className="HeroImg__list__item">
+			<img src={ArrowSVG} alt="" className="HeroImg__list__item__img" />
+			<p className="HeroImg__list__item__text">До 6,5% годовых</p>
 		</li>
-		<li className="HeroImgPluses__item">
-			<img src="" alt="icon" />
-			<p>До 6,5% годовых</p>
+		<li className="HeroImg__list__item">
+			<img src={ArrowSVG} alt="" className="HeroImg__list__item__img" />
+			<p className="HeroImg__list__item__text">
+				Пополнение и снятие&nbsp;<span className="inline">без ограничений</span>
+			</p>
 		</li>
-		<li className="HeroImgPluses__item">
-			<img src="" alt="icon" />
-			<p>До 6,5% годовых</p>
-		</li>
-		<li className="HeroImgPluses__item">
-			<img src="" alt="icon" />
-			<p>До 6,5% годовых</p>
+		<li className="HeroImg__list__item">
+			<img src={ArrowSVG} alt="" className="HeroImg__list__item__img" />
+			<p className="HeroImg__list__item__text">
+				Акция «Будь в плюсе» — возможность получить
+				<span className="inline">до 350 000 рублей</span>
+			</p>
 		</li>
 	</ul>
 )
 
 const HeroImgNav = () => (
-	<nav className="HeroImgNav">
-		<a href="/#">
-			<img src="" alt="OTP bank logo" />
+	<nav className="HeroImg__nav">
+		<a href="/#" className="HeroImg__nav__link">
+			<img
+				src={LogoSVG}
+				alt="OTP bank logo"
+				className="HeroImg__nav__link__img"
+			/>
 		</a>
 	</nav>
 )
@@ -35,11 +43,18 @@ const HeroImg = () => (
 	<section className="HeroImg">
 		<div className="container">
 			<HeroImgNav />
-			<h1 className="HeroImg__title">
-				Повышаем процент по накопительному счёту
-			</h1>
-			<HeroImgPluses />
-			<button className="HeroImg__btn btn btn--orange">Открыть счёт</button>
+			<div className="HeroImg__wrapper">
+				<h1 className="HeroImg__title">
+					Повышаем процент&nbsp;
+					<span className="HeroImg__title__inline">
+						по накопительному счёту
+					</span>
+				</h1>
+				<HeroImgPluses />
+				<Link to="Form" smooth={true} duration={500}>
+					<button className="HeroImg__btn btn btn--orange">Открыть счёт</button>
+				</Link>
+			</div>
 		</div>
 	</section>
 )
