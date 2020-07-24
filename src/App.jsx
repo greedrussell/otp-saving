b5c80da70417bea2874d5f1dc188fc596ee9b7c4
Russell Greed <greedrussell@gmail.com>
2020-07-24 14:41:22 +0300

    feat(how it work): add section how it work

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ActionInfo from './components/action-info/action-info.component'
 // import Footer from './components/footer/footer.component'
 import AboutAction from './components/about-action/about-action.components'
 import Calculator from './components/calculator/calculator.component'
-// import HowItWork from './components/how-it-work/how-it-work.component'
+import HowItWork from './components/how-it-work/how-it-work.component'
 // import CustomerInfo from './components/customer-info/customer-info.component'
 // import Faq from './components/faq/faq.component'
 // import MobileApp from './components/mobile-app/mobile-app.component'
@@ -18,7 +18,7 @@ const App = () => {
 			<ActionInfo />
 			<AboutAction />
 			<Calculator />
-			{/* <HowItWork /> */}
+			<HowItWork />
 			{/* <CustomerInfo /> */}
 
 			{/* <Faq /> */}
--- src/components/how-it-work/how-it-work.component.jsx
+++ src/components/how-it-work/how-it-work.component.jsx
@@ -1,20 +1,69 @@
-import React from '../customer-info/node_modules/react'
+import React from 'react'
 
 import './how-it-work.css'
+import PdfSVG from '../../assets/img/pdf.svg'
+import ChartDesktopPNG from '../../assets/img/how-it-work__desktop.png'
+import ChartMobilePNG from '../../assets/img/how-it-work__mobile.png'
 
 const HowItWork = () => (
 	<section className="HowItWork">
 		<div className="container">
-			<h2 className="title-h2 HowItWork__title">
-				Как работает накопительный счёт
-			</h2>
-			<p className="HowItWork__sub-title">
-				Ставки растут, в зависимости от суммы ежемесячных трат по карте «ОТП
-				Покупки»
-			</p>
-			<span className="HowItWork__text">Процент надбавки</span>
-			<span className="HowItWork__text">Ставка с учетом надбавки</span>
-			<span className="HowItWork__text">Сумма трат по карте </span>
+			<div className="HowItWork__top">
+				<h2 className="title-h2 HowItWork__top__title">
+					Как работает&nbsp;
+					<span className="HowItWork__top__title__inline">
+						накопительный счёт
+					</span>
+				</h2>
+				<p className="HowItWork__top__sub-title">
+					Ставки растут, в зависимости от суммы ежемесячных трат по карте «ОТП
+					Покупки»
+				</p>
+			</div>
+			<div className="HowItWork__middle">
+				<div className="HowItWork__middle__content">
+					<span className="HowItWork__middle__content__text">
+						Процент надбавки
+					</span>
+					<span className="HowItWork__middle__content__text">
+						Ставка с учетом надбавки
+					</span>
+					<span className="HowItWork__middle__content__text">
+						Сумма трат по карте
+					</span>
+				</div>
+				<div className="HowItWork__middle__img-block">
+					<img
+						className="HowItWork__middle__img-block__img HowItWork__middle__img-block__img--mobile"
+						src={ChartMobilePNG}
+						alt=""
+					/>
+					<img
+						className="HowItWork__middle__img-block__img HowItWork__middle__img-block__img--desktop"
+						src={ChartDesktopPNG}
+						alt=""
+					/>
+				</div>
+			</div>
+			<div className="HowItWork__bottom">
+				<p className="HowItWork__bottom__text">
+					Повышенная ставка начисляется на среднемесячный остаток по счёту,
+					<span className="HowItWork__bottom__text__inline">
+						не превышающий 350 000 рублей. Далее – начисление по базовой ставке
+						4%
+					</span>
+				</p>
+				<a href="#/" className="HowItWork__bottom__pdf-block">
+					<img
+						className="HowItWork__bottom__pdf-block__img"
+						src={PdfSVG}
+						alt="Иконка PDF файла"
+					/>
+					<p className="HowItWork__bottom__pdf-block__text">
+						Правила по накопительному счету
+					</p>
+				</a>
+			</div>
 		</div>
 	</section>
 )
